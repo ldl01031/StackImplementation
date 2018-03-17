26d437abb7e3d627e4d1abbaf5ec4417ea56d7a1
ldl01031 <Larry@ldlsoftware.com>
2018-03-16 20:27:29 -0700

    Added Clear() and Count()

--- src/main/StackImplementation.java
+++ src/main/StackImplementation.java
@@ -1,16 +1,29 @@
+import javax.naming.OperationNotSupportedException;
 import java.util.LinkedList;
 
-public class StackImplementation
-{
+public class StackImplementation {
+
     private LinkedList<Integer> _linkedList = new LinkedList<>();
 
     public void Push(int newValue) {
         _linkedList.add(newValue);
     }
 
-    public int Pop() {
+    public int Pop() throws OperationNotSupportedException {
+        if (Count() < 1) {
+            throw new OperationNotSupportedException("Cannot Pop from an empty stack.");
+        }
+
         int returnValue = _linkedList.getLast();
         _linkedList.removeLast();
         return returnValue;
+    }
+
+    public void Clear() {
+        _linkedList.clear();
+    }
+
+    public int Count() {
+        return _linkedList.size();
     }
 }
--- src/test/StackImplementationTest.java
+++ src/test/StackImplementationTest.java
@@ -1,24 +1,31 @@
 import org.junit.Assert;
-import org.testng.annotations.Test;
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.naming.OperationNotSupportedException;
 
 public class StackImplementationTest {
 
+    private StackImplementation myStack;
+
+    @Before
+    public void setUp() {
+        myStack = new StackImplementation();
+    }
+
     @Test
-    public void HappyPathTest()
-    {
+    public void HappyPathTest() throws OperationNotSupportedException {
         int stackValue1 = 42;
-        StackImplementation myStack = new StackImplementation();
         myStack.Push(stackValue1);
         int result = myStack.Pop();
         Assert.assertEquals(stackValue1, result);
     }
 
     @Test
-    public void PushMultiplePopMultipleSucceeds(){
+    public void PushMultiplePopMultipleSucceeds() throws OperationNotSupportedException {
         int stackValue1 = 42;
         int stackValue2 = 43;
         int stackValue3 = 44;
-        StackImplementation myStack = new StackImplementation();
         myStack.Push(stackValue1);
         myStack.Push(stackValue2);
         myStack.Push(stackValue3);
@@ -28,5 +35,27 @@ public class StackImplementationTest {
         Assert.assertEquals(stackValue2, result);
         result = myStack.Pop();
         Assert.assertEquals(stackValue1, result);
+    }
+
+    @Test
+    public void CountMethodSucceeds() {
+        Assert.assertEquals(0, myStack.Count());
+        myStack.Push(1);
+        myStack.Push(1);
+        Assert.assertEquals(2, myStack.Count());
+    }
+
+    @Test
+    public void ClearMethodClearsStackSucceeds() {
+        Assert.assertEquals(0, myStack.Count());
+        myStack.Push(1);
+        Assert.assertEquals(1, myStack.Count());
+        myStack.Clear();
+        Assert.assertEquals(0, myStack.Count());
+    }
+
+    @Test(expected = OperationNotSupportedException.class)
+    public void PopMethodOnEmptyStackReturnsException() throws OperationNotSupportedException {
+        myStack.Pop();
     }
 }
